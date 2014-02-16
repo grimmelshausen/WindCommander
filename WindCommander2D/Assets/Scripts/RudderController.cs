d81306c19573eb8c49ea1ded52b36098b019948e
grimmelshausen <fabiozuend@gmail.com>
2014-02-16 16:52:39 +0100

    Good algo for rudder rotation
    Finnaly found good way of smoothly rotate rudder. Same for sail would be
    good. Now it would actually make sense to use the gameobject rotation of
    the rudder to calculate forces. no? RudderController is now ON rudder
    and has ship as param

--- WindCommander2D/Assets/Scripts/RudderController.cs
+++ WindCommander2D/Assets/Scripts/RudderController.cs
@@ -1,32 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(Rigidbody2D))]
+
 public class RudderController : MonoBehaviour {
 
-
-	public Transform rudderHinge;
-
+	public float maxRudderAngle = 60;
+	public Rigidbody2D ship;
+	float input;
+	float rudderPercent;
 
 
 	// Use this for initialization
 	void Start () {
-	
+		rudderPercent = 0.5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        float input = -Input.GetAxis("Horizontal");
+        input = Input.GetAxis("Horizontal");
 
+		//Rotate rudder sprite
+		rudderPercent += input * Time.deltaTime; //calculate percentage of rudder 0 to 1 (0 is all left, 1 is all right)
+		rudderPercent = Mathf.Clamp01(rudderPercent); //make sure its not over 1 or below 0
+		float r = Mathf.SmoothStep(-maxRudderAngle, maxRudderAngle, rudderPercent);	//smoothly map 0 to 1 to -maxAngle to +maxAngle
+		this.transform.localRotation = Quaternion.Euler(0, 0, r); //set rotation to hinge (SET Z FOR 2D, Y FOR 3D)
 
-		// Rotate rudder sprite on hinge
-		rudderHinge.Rotate(0, 0, -input);
-
-
+		// Maybe it makes sense to use the hinge rotation now to calculate forces?
+	
         // Calculate forces and apply to rigidbody
         Vector2 keelPerp = new Vector2(1, 0);
-        Vector2 rudderPosition = new Vector2(0, -0.5f);
+         Vector2 rudderPosition = new Vector2(0, -0.5f);
         float shipAngle = Mathf.Deg2Rad * transform.rotation.eulerAngles.z;
         Vector2 rudderDirection = new Vector2(
             keelPerp.x * Mathf.Cos(shipAngle) - keelPerp.y * Mathf.Sin(shipAngle),
@@ -34,6 +38,6 @@ public class RudderController : MonoBehaviour {
         rudderPosition = new Vector2(
             rudderPosition.x * Mathf.Cos(shipAngle) - rudderPosition.y * Mathf.Sin(shipAngle),
             rudderPosition.x * Mathf.Sin(shipAngle) + rudderPosition.y * Mathf.Cos(shipAngle));
-        this.rigidbody2D.AddForceAtPosition((0.1f * input) * rudderDirection, new Vector2(transform.position.x, transform.position.y) + rudderPosition);
+		ship.AddForceAtPosition((0.1f * -input) * rudderDirection, new Vector2(transform.position.x, transform.position.y) + rudderPosition);
 	}
 }
