2c9a3c58376a44703ebded512b0f9a6fb9e55c8c
Mr. Threepwood <threep@brush.net>
2014-02-21 00:18:23 +0100

    merge test

--- WindCommander3D/Assets/Scripts/Mathhelp.cs
+++ WindCommander3D/Assets/Scripts/Mathhelp.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class Mathhelp {
 
+	//Diese yeile ist neu auf buero
 	public static Vector3 Clamp(Vector3 v, float min, float max)
 	{
 		return new Vector3(Mathf.Clamp(v.x, min, max),
 		          		  Mathf.Clamp(v.y, min, max),
-		          		  Mathf.Clamp(v.z, min, max));
+		          		  Mathf.Clamp(v.z, min, max)); //diese zeile ist modified auf buero
 	}
 }
