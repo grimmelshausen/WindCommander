23ed8c119e9dab3e5c9b2a584c6c2e5330b6b0c3
grimmelshausen <fabiozuend@gmail.com>
2014-04-06 17:44:56 +0200

    started new rudder controller, it is a bitch!!

---
+++ WindCommander3D/Assets/Scripts/ShipSailLutForce.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipSailLutForce : MonoBehaviour {
+
+	public Transform sail; //sail angle
+	public Transform wind; //wind angle
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	/**
+	 * Currently the wind strengh is ignored. We just need the rel wind angle and the sail angle
+	 */ 
+	void Update ()
+	{
+
+
+	
+	}
+}
