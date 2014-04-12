c240d07b1e0c7b032cc144c4bb80ca208893511c
grimmelshausen <fabiozuend@gmail.com>
2014-04-12 17:37:51 +0200

    Added blowing effect if ship is in irons

--- WindCommander3D/Assets/Scripts/Physics/SailBlow.cs
+++ WindCommander3D/Assets/Scripts/Physics/SailBlow.cs
@@ -16,6 +16,8 @@ public class SailBlow : MonoBehaviour {
    
     public Vector3 zeroPos = new Vector3(0, 2.411292f, -0.7563128f);
 
+    public SailRopeControllerThree sailRopeContoller;
+
 	public Vector3 blow;
 
 
@@ -26,6 +28,7 @@ public class SailBlow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 
         // The wind relative to the ship's velocity
         Vector3 relWind = wind.Wind() - ship.rigidbody.velocity;
@@ -43,9 +46,20 @@ public class SailBlow : MonoBehaviour {
                     dir = this.transform.right; break;
                 }
         }
-        blow = Vector3.Project(relWind, dir) * blowStrength;
-        blow = Quaternion.AngleAxis(-this.transform.rotation.eulerAngles.y, Vector3.up) * blow; //this is a hack, it rotates the vector by something. I have no fucking clue why this is needed and I found it by trial&error :p shit happens
-        this.transform.localPosition = blow + zeroPos;       
+        if (!sailRopeContoller.isInIrons)
+        {
+
+            blow = Vector3.Project(relWind, dir) * blowStrength;
+            blow = Quaternion.AngleAxis(-this.transform.rotation.eulerAngles.y, Vector3.up) * blow; //this is a hack, it rotates the vector by something. I have no fucking clue why this is needed and I found it by trial&error :p shit happens
+            this.transform.localPosition = blow + zeroPos;    
+        }
+        else
+        {
+            blow = dir * blowStrength * Mathf.Sin(Time.fixedTime*60);
+            blow = Quaternion.AngleAxis(-this.transform.rotation.eulerAngles.y, Vector3.up) * blow; //this is a hack, it rotates the vector by something. I have no fucking clue why this is needed and I found it by trial&error :p shit happens
+            this.transform.localPosition = blow + zeroPos; 
+        }
+
 
 	}
 }
